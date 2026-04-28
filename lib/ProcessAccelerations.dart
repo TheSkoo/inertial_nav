fc1949eca3f40d123907706a7feedd0b1119a606
Will Skou <WillSkou@gmail.com>
2026-04-28 08:41:42 -0500

    save point

--- lib/ProcessAccelerations.dart
+++ lib/ProcessAccelerations.dart
@@ -1,24 +1,28 @@
 import 'package:http/http.dart' as http; // Alias the package for convenience
 import 'package:flutter/services.dart';
-import 'dart:math' as math;
+import './axis_values.dart'; 
+
 
 class ProcessAccelerations {
   static const MethodChannel _channel = MethodChannel('flutter.native/helper');
   static const String logURL = "http://192.168.50.89:778/AndroidAccelerations";
   static BigInt lastMillisecond = BigInt.zero;
 
-  static double velocityX = 0.0;
-  static double velocityY = 0.0;
-  static double velocityZ = 0.0;
-  static double positionX = 0.0;
-  static double positionY = 0.0;
-  static double positionZ = 0.0;
-  static double deltaVx = 0.0;
-  static double previousAx = 0.0;
+  var xAxis = AxisValues();
+  var yAxis = AxisValues();
+  var zAxis = AxisValues();
 
-  double PositionX = 0.0;
-  double PositionY = 0.0;
-  double PositionZ = 0.0;
+  void integrateAccelerations(double mSec, double acceleration, AxisValues axisValues) {
+    double seconds = mSec / 1000.0;
+
+    double deltaV = ((acceleration + axisValues.previousAcceleration) / 2.0) * seconds;
+    axisValues.previousAcceleration = acceleration;
+
+    axisValues.velocity += deltaV;
+    
+  double deltaP = axisValues.velocity * seconds;
+  axisValues.position += deltaP;
+}
 
   void Initialize() {
     _channel.setMethodCallHandler(nativeMethodCallHandler);
@@ -38,14 +42,17 @@ class ProcessAccelerations {
         
         if (lastMillisecond != BigInt.zero) {
           var p1 = BigInt.parse(accelData['ts'].toString()) - lastMillisecond;
-          var p2 = double.parse(accelData['x'].toString());
-          var p3 = double.parse(accelData['y'].toString());
-          var p4 = double.parse(accelData['z'].toString());
+          LogSquat("squatly");
+          var ax = double.parse(accelData['x'].toString());
+          var ay = double.parse(accelData['y'].toString());
+          var az = double.parse(accelData['z'].toString());
 
           lastMillisecond = BigInt.parse(accelData['ts'].toString());
           final mSec = p1.toDouble() / 1000000.0;
-          //Log(p1, p2, p3, p4, mSec);
-          integrateAccelerations(mSec, p2, p3, p4);
+          integrateAccelerations(mSec, ax, xAxis);
+          Log(p1, ax, ay, az, mSec);
+          integrateAccelerations(mSec, ay, yAxis);
+          integrateAccelerations(mSec, az, zAxis);
         }
         else {
           lastMillisecond = BigInt.parse(accelData['ts'].toString());
@@ -56,46 +63,8 @@ class ProcessAccelerations {
     }
   }
 
-var isRunning = false;
-void integrateAccelerations(double mSec, double x, double y, double z) {
-  if (isRunning) {
-    LogSquat("ReEntered");
-  }
-
-  isRunning = true;
-  try{
-  double seconds = mSec / 1000.0;
-
-  deltaVx = (((x + previousAx) / 2.0) - 0.0012145351910708) * seconds;
-  previousAx = x;
-
-  double deltaVy = y * seconds;
-  double deltaVz = z * seconds;
- 
-  velocityX += deltaVx;
-  velocityY += deltaVy;
-  velocityZ += deltaVz;
-    
-  double deltaPx = velocityX * seconds;
-  double deltaPy = velocityY * seconds;
-  double deltaPz = velocityZ * seconds;
-  positionX += deltaPx;
-  positionY += deltaPy;
-  positionZ += deltaPz;
-  
-   PositionX = positionX;
-   PositionY = convertPosition(positionY);
-   PositionZ = convertPosition(positionZ);
-   Log(BigInt.from(mSec.toInt()), x, deltaVx, velocityX, seconds);
-}
-finally {
-isRunning = false;
-}
-  }
-
 double convertPosition(double pos) {
   var result = pos * 39.3701;
-  result = (result * math.pow(10, 2)).truncate() / math.pow(10, 2);
   return result;
 }
 
